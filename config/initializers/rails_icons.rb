61530a7178040a1620d9ae1ef7a5b2b135b528bf
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2025-01-03 20:08:21 +0900

    add flash partial

---
+++ config/initializers/rails_icons.rb
@@ -0,0 +1,9 @@
+RailsIcons.configure do |config|
+  config.default_library = "feather"
+  # config.default_variant = "" # Set a default variant if multiple exist
+
+  # Override Feather defaults
+  # config.libraries.feather.default.css = "size-6"
+  # config.libraries.feather.default.stroke_width = "2"
+  # config.libraries.feather.default.data = {}
+end
